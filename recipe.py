2818b6549bb15fec566af53d97d89e3e7b13b844
FaithfulWater <faithfulwater@gmail.com>
2017-08-07 15:56:15 -0400

    Create recipe.py

---
+++ recipe.py
@@ -0,0 +1,12 @@
+ingridients = [' one cup of frozen raspberries', '1/2 cup of fresh mint leaves', '1 tsp granulated sugar', '3/4 cup of white rum', '1 tsp fresh lime juice', '1 750 millimeter bottle of Lillet','Ice','1 cup Seltzer']
+
+directions =['Mash the raspberries with the mint and sugar in large bowl until raspberries are broken down into small pieces', 'Add the rum, lime juice, and Lille tand stir to combine', 'cover and refrigerate for 1 to 4 hours','strain mixture into pitcher with ice', 'serve immediately']
+
+drink_recipie = {'ingridients' : ingridients, 'directions' : directions}
+
+cookbook = {'drink' : drink_recipie}
+
+print(cookbook)
+
+#for tbd in range()
+    #print 
